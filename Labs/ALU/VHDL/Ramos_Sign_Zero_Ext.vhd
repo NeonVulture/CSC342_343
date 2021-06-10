dd90f85a1fca21ff87e1801180d53623c0082af7
Anthony Ramos <47514287+NeonVulture@users.noreply.github.com>
2021-06-10 11:18:53 -0400

    Sign-Zero Extender

---
+++ Labs/ALU/VHDL/Ramos_Sign_Zero_Ext.vhd
@@ -0,0 +1,13 @@
+Library IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.NUMERIC_STD.ALL;
+
+Entity Ramos_Sign_Zero_Ext is
+    port( A     : in std_logic_vector(15 downto 0);
+          Output: out std_logic_vector(31 downto 0));
+end Ramos_Sign_Zero_Ext;
+
+architecture behvaioral of Ramos_Sign_Zero_Ext is 
+begin
+    Output <= std_logic_vector(resize(signed(A), 32));
+end behvaioral;
